99dd785b461593e2e4956192c13b6c5841577d2a
Hammad Habib <hammadhabib80@gmail.com>
2024-03-29 09:04:23 +0500

    Commiting after updating the admin-sdk, shifted to a new database :(

--- firebase/riderDB.js
+++ firebase/riderDB.js
@@ -15,36 +15,36 @@ module.exports.register = async (name,cnic,phone,address,email, password) => {
     );
     const riderObj = rider.toObject();
 
-    var querySnapShot = await firestore.collection("rider").where("cnic", "==", cnic).get();
-    if (!querySnapShot.empty) {
-        return {
-            status: false,
-            message: "Rider with this cnic already exists",
-            uid: null,
-        }
-    }
+    // var querySnapShot = await firestore.collection("rider").where("cnic", "==", cnic).get();
+    // if (!querySnapShot.empty) {
+    //     return {
+    //         status: false,
+    //         message: "Rider with this cnic already exists",
+    //         uid: null,
+    //     }
+    // }
 
-    querySnapShot = await firestore.collection("rider").where("phone", "==", phone).get();
-    if (!querySnapShot.empty) {
-        return {
-            status: false,
-            message: "Rider with this phone already exists",
-            uid: null,
-        }
-    }
+    // querySnapShot = await firestore.collection("rider").where("phone", "==", phone).get();
+    // if (!querySnapShot.empty) {
+    //     return {
+    //         status: false,
+    //         message: "Rider with this phone already exists",
+    //         uid: null,
+    //     }
+    // }
 
   try {
-    const userRecord = await auth.createUser({
-      email: email,
-      password: password,
-    });
+    // const userRecord = await auth.createUser({
+    //   email: email,
+    //   password: password,
+    // });
     
     const riderRef = await firestore.collection("rider").add(riderObj);
 
     return {
         status: true,
         message: "Successfully created new user",
-        uid: userRecord.uid,
+        // uid: userRecord.uid,
     }
   } catch (error) {
     const errorCode = error.code;
